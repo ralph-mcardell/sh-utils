551f868390e5f5c754694e9b92a4948bd0950fff
Ralph E. McArdell <ralph@dibase.co.uk>
2021-04-15 13:41:10 +0100

    Update comment header and correct spelling seperator/SEPERATOR -> setarator/SEPARATOR

--- lib/dict.sh
+++ lib/dict.sh
@@ -8,6 +8,16 @@
 # as well as most unprintable characters except ASCII FS, GS, RS and US.
 # These are reserved for dict implementation purposes.
 #
+# Values can also be other dict (formatted strings), that is dicts can be
+# nested in dicts.
+#
+# As hinted above the implementation is a specially formatted string which
+# uses ASCII separator control code values to separate key from value within
+# an entry and entries from each other. Entries are stored in the order they
+# are added so dicts are unsorted. As manipulation requries string pattern
+# matching, cutting, pasting and (for nesting / unnesting) subsitution via sed
+# do not expect any decent perforance.
+#
 # Use:
 # dict_set      to add or update key,value to blank string or existing
 #               dict-formatted variable previously returned from dist_set.
@@ -15,22 +25,31 @@
 #               variable
 # dict_remove   to remove the key,value entry in a dict associated with a key.
 # dict_to_vars  to convert each key,value in a dict to a variable
-
+# dict_print_raw to print the raw string of a dict variable with substitutions
+#               for the US, RS, GS and FS non-printing separator characters.
+#               Useful for debugging and similar.
+#
+# sdict_set, sdict_get, sdict_remove, sdict_to_vars are simpler versions that
+# do not support nesting of dict values in other dict variables. They should
+# have less overhead (sdict_set and sdict_get in particular require
+# substitutions of key,value and entry separator sequences using sed to allow
+# dicts to be nested as dict values).
+#
 
 __DICT_FS__=$(echo '@' | tr '@' '\034')
 __DICT_GS__=$(echo '@' | tr '@' '\035')
 __DICT_RS__=$(echo '@' | tr '@' '\036')
 __DICT_US__=$(echo '@' | tr '@' '\037')
 
-__DICT_RECORD_SEPERATOR__="${__DICT_RS__}"
-__DICT_FIELD_SEPERATOR__="${__DICT_US__}"
+__DICT_RECORD_SEPARATOR__="${__DICT_RS__}"
+__DICT_FIELD_SEPARATOR__="${__DICT_US__}"
 __DICT_NESTING_PREFIX__="${__DICT_GS__}"
 
-__DICT_ENTRY_SEPERATOR__="${__DICT_FIELD_SEPERATOR__}${__DICT_RECORD_SEPERATOR__}"
+__DICT_ENTRY_SEPARATOR__="${__DICT_FIELD_SEPARATOR__}${__DICT_RECORD_SEPARATOR__}"
 
 __dict_decorated_key__() {
     cat << EOF 
-${1}${__DICT_FIELD_SEPERATOR__}
+${1}${__DICT_FIELD_SEPARATOR__}
 EOF
 }
 
@@ -38,7 +57,7 @@ __dict_new_entry__() {
     local decorated_key="${1}"
     local value="${2}"
     cat << EOF 
-${decorated_key}${value}${__DICT_ENTRY_SEPERATOR__}
+${decorated_key}${value}${__DICT_ENTRY_SEPARATOR__}
 EOF
 }
 
@@ -64,7 +83,7 @@ __dict_value__() {
     local value_plus="$(__dict_value_and_suffix_entries__ "${dict}" "${decorated_key}")"
     if [ "${value_plus}" != "${dict}" ]; then
         cat << EOF
-${value_plus%%${__DICT_ENTRY_SEPERATOR__}*}
+${value_plus%%${__DICT_ENTRY_SEPARATOR__}*}
 EOF
     fi
 }
@@ -74,7 +93,7 @@ __dict_suffix_entries__() {
     local decorated_key="${2}"
     local value_plus="$(__dict_value_and_suffix_entries__ "${dict}" "${decorated_key}")"
     if [ "${value_plus}" != "${dict}" ]; then
-        suffix_entries="${value_plus#*${__DICT_ENTRY_SEPERATOR__}}"
+        suffix_entries="${value_plus#*${__DICT_ENTRY_SEPARATOR__}}"
         if [ "${suffix_entries}" = "${value_plus}" ]; then
             suffix_entries=''
         fi
@@ -138,10 +157,10 @@ EOF
 sdict_to_vars() {
     local dict="${1}"
     while [ -n "${dict}" ]; do
-        local record="${dict%%${__DICT_ENTRY_SEPERATOR__}*}"
-        local key="${record%${__DICT_FIELD_SEPERATOR__}*}"
-        local value="${record#*${__DICT_FIELD_SEPERATOR__}}"
-        local dict="${dict#*${__DICT_ENTRY_SEPERATOR__}}"
+        local record="${dict%%${__DICT_ENTRY_SEPARATOR__}*}"
+        local key="${record%${__DICT_FIELD_SEPARATOR__}*}"
+        local value="${record#*${__DICT_FIELD_SEPARATOR__}}"
+        local dict="${dict#*${__DICT_ENTRY_SEPARATOR__}}"
 #        echo "dict:\"${dict}\" record:\"${record}\" key:\"${key}\" value:\"${value}\"" | tr "${__DICT_RS__}${DICT___DICT_US__}" '^_' >&2
         read ${key} << EOF 
 ${value}
