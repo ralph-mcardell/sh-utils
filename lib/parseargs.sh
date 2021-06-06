ed48cab24163cd9ca07ba3a1a9f6980ffceb6f59
Ralph E. McArdell <ralph@dibase.co.uk>
2021-06-06 12:45:57 +0100

    Make initial simple test pass  -  add additional data (and handling logic) to parser to    track positional, long and short optional arguments  - add initial very simple  positional arguments only    parseargs_parse_arguments.

--- lib/parseargs.sh
+++ lib/parseargs.sh
@@ -19,6 +19,9 @@ then
 
   parseargs_new_argument_parser() {
     local initial_arguments="$(dict_declare_simple)"
+    local positionals="$(dict_declare_simple)"
+    local longopts="$(dict_declare_simple)"
+    local shortopts="$(dict_declare_simple)"
     local parser="$(dict_declare_simple \
                      "__PARSEARG_TYPE__" "argument_parser"
                   )"
@@ -32,26 +35,66 @@ then
     done
     
     parser="$(dict_set "${parser}" "__arguments__" "${initial_arguments}")"
+    parser="$(dict_set "${parser}" "__positionals__" "${positionals}")"
+    parser="$(dict_set "${parser}" "__longopts__" "${longopts}")"
+    parser="$(dict_set "${parser}" "__shortopts__" "${shortopts}")"
     echo -n "${parser}"
   }
 
   parseargs_add_argument() {
-   __parseargs_abort_if_not_parser__ "${1}" "parseargs_add_argument"
+    __parseargs_abort_if_not_parser__ "${1}" "parseargs_add_argument"
     local argument="$(dict_declare_simple \
                      "__PARSEARG_TYPE__" "argument_parser_argument"
                   )"
     local parser="${1}"
+    local positionals="$(dict_get "${parser}" "__positionals__")"
+    local longopts="$(dict_get "${parser}" "__longopts__")"
+    local shortopts="$(dict_get "${parser}" "__shortopts__")"
+    local optstring="$(dict_get "${parser}" "__optstring__")"
     shift
     while [ "$#" -gt "1" ]; do
       case ${1} in
         name)
-          argument="$(dict_set_simple "${argument}" "name" "${2}")"
+          if [ -z "${is_option}" ]; then
+            if [ -z "${positional}" ]; then
+              positional="${2}"
+            else
+              echo "ERROR: Argument name attribute specified more than once." >&2
+              exit 1
+            fi
+          else
+            echo "ERROR: Both name and long/short specified. Argument cannot be both positional and optional." >&2
+            exit 1
+          fi
           ;;
         short)
-          argument="$(dict_set_simple "${argument}" "short" "${2}")"
+          if [ -z "${positional}" ]; then
+            if [ -z "${short_opt}" ]; then
+              is_option="true"
+              short_opt="${2}"
+            else
+              echo "ERROR: Argument short attribute specified more than once." >&2
+              exit 1
+            fi
+          else
+            echo "ERROR: Both name and long/short specified. Argument cannot be both positional and optional." >&2
+            exit 1
+          fi
           ;;
         long)
-          argument="$(dict_set_simple "${argument}" "long" "${2}")"
+          if [ -z "${positional}" ]; then
+            if [ -z "${long_opt}" ]; then
+              argument="$(dict_set_simple "${argument}" "long" "${2}")"
+              is_option="true"
+              long_opt="${2}"
+            else
+              echo "ERROR: Argument long attribute specified more than once." >&2
+              exit 1
+            fi
+          else
+            echo "ERROR: Both name and long/short specified. Argument cannot be both positional and optional." >&2
+            exit 1
+          fi
           ;;
         destination)
           argument="$(dict_set_simple "${argument}" "destination" "${2}")"
@@ -65,11 +108,12 @@ then
       esac
       shift 2
     done
+
     local dest=$(dict_get_simple "${argument}" "destination")
     if [ -z "${dest}" ]; then
-      dest="$(dict_get_simple "${argument}" "name")"
+      dest="${positional}"
       if [ -z "${dest}" ]; then
-        dest="$(dict_get_simple "${argument}" "long")"
+        dest="${long_opt}"
       fi
     fi
     dest="$(__parseargs_sanitise_destination__ "${dest}")"
@@ -78,14 +122,80 @@ then
       echo "Error: unable to deduce destination name for argument value." >&2
       exit 1
     fi
+
+    if [ -n "${positional}" ]; then
+      argument="$(dict_set_simple "${argument}" "name" "${2}")"
+      positionals="$(dict_set_simple "${positionals}" "$(dict_size "${positionals}")" "${dest}")"
+    elif  [ -n "${is_option}" ]; then
+      if [ -n "${short_opt}" ]; then
+        if [ "${#short_opt}" -ne 1 ]; then
+          echo "Error: Argument short option attribute value '${short_opt}' is not a single character." >&2
+          exit 1
+        fi
+        existing="$(dict_get_simple "${shortopts}" "${short_opt}")"
+        if [ -n "${existing}" ]; then
+          echo "Error: Argument short option attribute value '${short_opt}' given previously." >&2
+          exit 1
+        fi
+        argument="$(dict_set_simple "${argument}" "short" "${short_opt}")"
+        shortopts="$(dict_set_simple "${shortopts}" "${short_opt}" "${dest}")"
+        optstring="${optstring}${short_opt}"
+      fi
+      if [ -n "${long_opt}" ]; then
+        existing="$(dict_get_simple "${longopts}" "${long_opt}")"
+        if [ -n "${existing}" ]; then
+          echo "Error: Argument long option attribute value '${long_opt}' given previously." >&2
+          exit 1
+        fi
+        argument="$(dict_set_simple "${argument}" "long" "${long_opt}")"
+        longopts="$(dict_set_simple "${longopts}" "${long_opt}" "${dest}")"
+      fi
+    else
+      echo "Error: none of name, long or short attributes provided for argument." >&2
+      exit 1
+    fi
+
     argument="$(dict_set_simple "${argument}" "destination" "${dest}")"
 
     args="$(dict_get "${parser}" "__arguments__")"
-    args="$(dict_set "${args}" "${argument}")"
+    args="$(dict_set "${args}" "${dest}" "${argument}")"
     parser="$(dict_set "${parser}" "__arguments__" "${args}")"
+    parser="$(dict_set "${parser}" "__positionals__" "${positionals}")"
+    parser="$(dict_set "${parser}" "__longopts__" "${longopts}")"
+    parser="$(dict_set_simple "${parser}" "__optstring__" "${optstring}")"
+
     echo -n "${parser}"
   }
 
+  parseargs_parse_arguments() {
+    __parseargs_abort_if_not_parser__ "${1}" "parseargs_parse_arguments"
+    local parser="${1}"
+    local arg_specs="$(dict_get "${parser}" "__arguments__")"
+    local positionals="$(dict_get "${parser}" "__positionals__")"
+    local longopts="$(dict_get "${parser}" "__longopts__")"
+    local shortopts="$(dict_get "${parser}" "__shortopts__")"
+    local optstring="$(dict_get_simple "${parser}" "__optstring__")"
+    local current_positional="0"
+
+    arguments="$(dict_declare_simple "__PROGRAM__" "${0}")"
+    shift
+    while [ "$#" -gt "0" ]; do
+      dest="$(dict_get_simple "${positionals}" "${current_positional}")"
+      if [ -z "${dest}" ]; then
+        echo "Too many positional arguments provided, remaining ignored." >&2
+        echo -n "${arguments}"
+      fi
+      current_positional=$((${current_positional}+1))
+      attributes="$(dict_get "${arg_specs}" "${dest}")"
+      if [ -z "${attributes}" ]; then
+        echo "Error (internal). No attrubutes specifying this argument." >&2
+        exit 1
+      fi
+      arguments="$(dict_set_simple "${arguments}" "${dest}" "${1}")"
+      shift
+    done
+    echo -n "${arguments}"
+  }
 
   __parseargs_abort_if_not_parser__() {
     if ! parseargs_is_argument_parser "${1}"; then
